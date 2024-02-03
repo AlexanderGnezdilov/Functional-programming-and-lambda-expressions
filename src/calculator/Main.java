e7e15c181be077ee1a850c0ba0b52870736b0605
Alexander Gnezdilov <alex.bah2022@yandex.ru>
2024-02-04 00:38:15 +0300

    Initial commit

---
+++ src/calculator/Main.java
@@ -0,0 +1,12 @@
+package calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1, 1);
+        int c = calc.divide.apply(a, b);//Ошибка в результате деления на ноль.
+        // Исправил переменную divide в классе Calculator
+        calc.println.accept(c);
+    }
+}
